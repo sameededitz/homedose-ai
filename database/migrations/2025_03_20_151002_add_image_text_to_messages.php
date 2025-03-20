51b99c4650f674bacc7bb304344c35590a3e068f
Sameed <sameedhassan22@gmail.com>
2025-03-20 15:10:34 +0500

    Remove image_text column from messages table in migration rollback

--- database/migrations/2025_03_20_151002_add_image_text_to_messages.php
+++ database/migrations/2025_03_20_151002_add_image_text_to_messages.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('messages', function (Blueprint $table) {
-            //
+            $table->dropColumn('image_text');
         });
     }
 };
